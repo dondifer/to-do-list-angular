1f295f1e0770acc61062f5edcc1a7e532e6d869e
dondifer <dondifer@gmail.com>
2024-01-11 21:40:32 +0100

    feat: list

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { Observable, of } from 'rxjs';
 interface Todo {
   content: string;
@@ -11,10 +12,30 @@ interface Todo {
   styleUrl: './list.component.scss',
 })
 export class ListComponent implements OnInit {
-  todoList: Observable<Todo[]> = of([
-    { id: 1, content: 'compras', isDone: false },
+  list = [
+    { id: 0, content: 'compras', isDone: false },
     { id: 1, content: 'pasear perro', isDone: false },
-  ]);
+  ];
+  todoList: Observable<Todo[]> = of(this.list);
 
   ngOnInit() {}
+
+  constructor(public snackBar: MatSnackBar) {}
+
+  markItemAsDone(item: any) {
+    this.list[item.id].isDone = true;
+    this.todoList = of(this.list);
+    this.openSnackBar('Item Done!', 'Dismiss');
+  }
+  markItemAsNotDone(item: any) {
+    this.list[item.id].isDone = false;
+    this.todoList = of(this.list);
+    this.openSnackBar('Item Not Done!', 'Dismiss');
+  }
+  openSnackBar(message: string, action: string) {
+    this.snackBar.open(message, action, {
+      duration: 2000,
+      verticalPosition: 'top',
+    });
+  }
 }
